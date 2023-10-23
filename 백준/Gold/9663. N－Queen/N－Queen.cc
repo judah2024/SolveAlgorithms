cb950032096ff8a50dab2d8a96010ae7febe4dcd
judah2023 <124682191+judah2023@users.noreply.github.com>
2023-10-23 11:32:37 +0900

    [Gold IV] Title: N-Queen, Time: 3360 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Gold/9663. N－Queen/N－Queen.cc
@@ -0,0 +1,67 @@
+#include <iostream>
+#include <vector>
+#include <cmath>
+
+using namespace std;
+
+bool isPlacedQueen(vector<int>& queens, vector<int>& isVisitedHorizontal, const int count, const int k)
+{
+    if (isVisitedHorizontal[k]) return false;
+
+    for (int i = 0; i < queens.size(); i++)
+    {
+        int gapH = abs(count - i);
+        int gapV = abs(k - queens[i]);
+
+        if (gapH == gapV) return false;
+    }
+
+    return true;
+}
+
+int BTQueen(vector<int>& queens, vector<int>& isVisitedHorizontal, const int n, const int count)
+{
+    if (count == n)
+    {
+        return 1;
+    }
+
+    int total = 0;
+    for (int i = 0; i < n; i++)
+    {
+        if (isPlacedQueen(queens, isVisitedHorizontal, count, i))
+        {
+            queens.push_back(i);
+            isVisitedHorizontal[i] = true;
+
+            total += BTQueen(queens, isVisitedHorizontal, n, count + 1);
+
+            isVisitedHorizontal[i] = false;
+            queens.pop_back();
+        }
+    }
+
+    return total;
+}
+
+int CalculateQueenArrangement(const int n)
+{
+    vector<int> queens;
+    vector<int> isVisitedHorizontal(n, false);
+
+    queens.reserve(n);
+
+    return BTQueen(queens, isVisitedHorizontal, n, 0);
+}
+
+int main()
+{
+    ios_base::sync_with_stdio(false); cin.tie(nullptr);
+
+    int n;
+    cin >> n;
+
+    cout << CalculateQueenArrangement(n);
+
+    return 0;
+}
