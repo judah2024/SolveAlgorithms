640cf1aa59a347e27bbf10c29cb65a340e40dd78
judah2023 <124682191+judah2023@users.noreply.github.com>
2023-10-29 00:35:25 +0900

    [Gold V] Title: 선 긋기, Time: 364 ms, Memory: 9836 KB -BaekjoonHub

--- 백준/Gold/2170. 선 긋기/선 긋기.cc
+++ 백준/Gold/2170. 선 긋기/선 긋기.cc
@@ -3,34 +3,26 @@
 #include <algorithm>
 
 using namespace std;
-
 using PII = pair<int, int>;
 
-vector<PII> InitializeLines(const int n)
+int main()
 {
-    vector<PII> lines(n);
+    ios_base::sync_with_stdio(false); cin.tie(nullptr);
 
+    int n;
+    cin >> n;
+
+    vector<PII> lines(n);
     for (int i = 0; i < n; i++)
     {
         int s, e;
         cin >> s >> e;
 
-        if (s > e)
-        {
-            swap(s, e);
-        }
-
         lines[i] = { s, e };
     }
     sort(lines.begin(), lines.end());
-    
-    return lines;
-}
 
-long long CalculateLinesLength(const vector<PII>& lines)
-{
     long long length = 0;
-    
     int lineEnd = lines[0].first;
     for (auto& line : lines)
     {
@@ -49,18 +41,7 @@ long long CalculateLinesLength(const vector<PII>& lines)
         lineEnd = max(e, lineEnd);
     }
 
-    return length;
-}
-
-int main()
-{
-    ios_base::sync_with_stdio(false); cin.tie(nullptr);
-
-    int n;
-    cin >> n;
-
-    vector<PII> lines = InitializeLines(n);
-    cout << CalculateLinesLength(lines);
+    cout << length;
 
     return 0;
 }
