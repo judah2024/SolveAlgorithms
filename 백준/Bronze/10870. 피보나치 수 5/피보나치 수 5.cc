79a3c0c5ffa8f8ebe0cdbcd4644bae45f0b58a29
judah2023 <124682191+judah2023@users.noreply.github.com>
2023-11-02 01:23:36 +0900

    [Bronze II] Title: 피보나치 수 5, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/10870. 피보나치 수 5/피보나치 수 5.cc
@@ -0,0 +1,21 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    ios_base::sync_with_stdio(false); cin.tie(nullptr);
+
+    int n;
+    cin >> n;
+
+    long long a = 0, b = 1;
+    for (int i = 0; i < n; i++)
+    {
+        swap(a, b);
+        b += a;
+    }
+
+    cout << a;
+    return 0;
+}
