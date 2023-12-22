525d8e759c6c27b7e0ab98b9403dda8d668b0359
judah2023 <124682191+judah2023@users.noreply.github.com>
2023-12-22 23:33:43 +0900

    [level 2] Title: 가장 큰 수, Time: 0.70 ms, Memory: 4.21 MB -BaekjoonHub

---
+++ 프로그래머스/2/42746. 가장 큰 수/가장 큰 수.cpp
@@ -0,0 +1,45 @@
+#include <string>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+bool Compare(const string& lhs, const string& rhs)
+{
+    string a = lhs + rhs;
+    string b = rhs + lhs;
+
+    if (a == b)
+        return lhs > rhs;
+
+    return a > b;
+}
+
+string solution(vector<int> numbers) {
+    vector<int> countSet(1001, 0);
+    for (auto& n : numbers)
+    {
+        countSet[n]++;
+    }
+    
+    if (countSet[0] == numbers.size())
+        return "0";
+
+    vector<string> nDict(1001);
+    for (int i = 0; i <= 1000; i++)
+    {
+        nDict[i] = to_string(i);
+    }
+    sort(nDict.begin(), nDict.end(), Compare);
+
+    string answer = "";
+    for (auto& it : nDict)
+    {
+        while (countSet[stoi(it)]--)
+        {
+            answer += it;
+        }
+    }
+
+    return answer;
+}
