b5438e026117d776b0b41dc6659aa52bb03aa9b3
judah2023 <124682191+judah2023@users.noreply.github.com>
2024-02-19 15:19:15 +0900

    [level 1] Title: 카드 뭉치, Time: 0.01 ms, Memory: 3.67 MB -BaekjoonHub

---
+++ 프로그래머스/1/159994. 카드 뭉치/카드 뭉치.cpp
@@ -0,0 +1,19 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+string solution(vector<string> cards1, vector<string> cards2, vector<string> goal) {
+    string answer = "";
+    int i = 0, j = 0;
+    for (auto& word : goal)
+    {
+        if (i != cards1.size() && cards1[i] == word)
+            i++;
+        else if (j != cards2.size() && cards2[j] == word)
+            j++;
+        else
+            return "No";
+    }
+    return "Yes";
+}
